131dcd760dc3d4fa6eb1acdedecabc48af855d78
José Gallardo <ing_gallardo@outlook.com>
2019-10-19 10:52:45 -0500

    Blog2 and Blog3 completed

--- resources/views/blogs/blog2.blade.php
+++ resources/views/blogs/blog2.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', ' - ¿El fin del yeso para inmovilizar extremidades?')
+@section('title', ' - Stop the bleed')
 
 @section('content')
 <!-- breadcrumb start-->
@@ -9,8 +9,8 @@
             <div class="col-lg-12">
                 <div class="breadcrumb_iner">
                     <div class="breadcrumb_iner_item">
-                        <h2>¿El fin del yeso para inmovilizar extremidades?</h2>
-                        <p>Inicio<span>/</span>el-fin-del-yeso-para-inmovilizar-extremidades</p>
+                        <h2>Stop the bleed</h2>
+                        <p>Inicio<span>/</span>stop-the-bleed</p>
                     </div>
                 </div>
             </div>
@@ -25,28 +25,35 @@
             <div class="col-lg-8 posts-list">
                 <div class="single-post">
                     <div class="feature-img">
-                        <img class="img-fluid" src="img/blog/single_blog_1.png" alt="">
+                        <img class="img-fluid" src="img/blog/single_blog_2.png" alt="">
                     </div>
                     <div class="blog_details">
-                        <h2>Médicos en Escocia desarrollaron una nueva técnica quirúrgica que podría significar el fin de las férulas de yeso para ciertas lesiones en las extremidades.
+                        <h2>En el caso de sangrado intenso, toma estas medidas de primeros auxilios y tranquiliza a la persona lesionada.
                         </h2>
                         <p class="excert">
-                            El procedimiento consiste en un soporte interno que se introduce en el cuerpo con una laparoscopia, una cirugía mínimamente invasiva. Actualmente, cuando ocurre una fractura u otra lesión en alguna extremidad se utiliza una preparación de yeso para inmovilizarla. Pero el profesor Gordon Mackay, cirujano ortopédico del Hospital Ross Hall, en Glasgow, deseaba encontrar una forma de evitar el desgaste muscular que provoca el enyesado y la inconveniencia de tener que usar cabestrillos y botas especiales. 
+                            Retira todas las prendas o restos de la herida. No quites objetos grandes o que estén incrustados profundamente. No explores la herida ni intentes limpiarla aún. Tu primera tarea es detener el sangrado. Usa guantes de protección desechables si puedes conseguirlos. 
                         </p>
                         <div class="quote-wrapper">
                             <div class="quotes">
-                                "Creo que todos los que han pasado por la experiencia de tener que utilizar una aguja de tejer e introducirla dentro del enyesado para aliviar la picazón, saben que las férulas son extremadamente desagradables" dice el especialista. "También, cuando llega el momento de retirarla, la extremidad tiende a enconarse y los músculos sufren un desgaste enorme", agrega.
+                                "En los casos de sangrado intenso que no puedas controlar, llama al 911 o a un servicio de emergencia médica."
                             </div>
                         </div>
-                        <h2>Ligamentos dañados</h2>
                         <p>
-                            Para evitar todos estos problemas el profesor Mackay utiliza una cirugía de laparsocopia -que requiere una pequeña incisión- para insertar en la una pieza de cinta que actúa como una abrazadera sobre el tejido lesionado. La técnica, por ahora, está siendo probada con ligamentos que sufrieron esguinces y que actualmente requieren inmovilizarse con yeso.
+                            Detén el sangrado. Coloca una venda estéril o un paño limpio sobre la herida. Presiona la venda con firmeza con la palma de la mano para controlar el sangrado. Aplica presión constante hasta que el sangrado se detenga. Mantén la presión sujetando bien la herida con una venda gruesa o con un trozo de paño limpio. No ejerzas presión directa sobre una lesión ocular o un objeto incrustado.
+                            Asegura la venda con cinta adhesiva o continúa manteniendo la presión con las manos. Si es posible, levanta la extremidad lesionada por encima del nivel del corazón.
                         </p>
                         <p>
-                            La abrazadera, como explica el profesor Mackay, permite el movimiento del tejido pero al mismo tiempo ofrece un apoyo en el ligamente mientras está sanando. Esto significa que el paciente no necesita estar inmovilizado y puede recuperarse mucho más rápido. La técnica es de particular interés para deportistas y atletas que muchas veces deben pasar semanas inmovilizados recuperándose de ligamentos dañados. 
+                            Ayuda a la persona lesionada a recostarse. Si es posible, coloca a la persona sobre una alfombra o una manta para evitar la pérdida de calor corporal. Tranquiliza a la persona lesionada. 
                         </p>
-                        <p>Una de las pacientes del profesor Mackay es la patinadora olímpica Sinead Kerr, quien se lesionó cuando patinaba y su pareja artística cayó sobre ella. "No he tenido dolor desde que fui sometida a la cirugía y estoy tomándolo paso por paso. Pero gradualmente estoy volviendo a la normalidad", dice Kerr. "Creo que me ha ayudado mucho el hecho de que no tenido que cargar algo excesivamente rígido en mi brazo". "Y eso me ha permitido llevar a cabo movimientos". </p>
-                        <p>El profesor Mackay ya ha llevado a cabo el procedimiento en unos 20 pacientes en Reino Unido y ahora está colaborando con la Clínica Steadman, en Estados Unidos, que se especializa en el tratamiento de heridas de estrellas deportivas. "Si el paciente pude volver a moverse mucho más rápido no desarrolla los problemas secundarios y estará mucho más cómodo". "Además, puede comenzar su rehabilitación de forma inmediata", señala el experto.</p>
+                        <p>
+                            No le quites la gasa o la venda. Si el sangrado se filtra a través de la gasa o de otro paño sobre la herida, agrega otra venda por encima. Y continúa presionando con firmeza sobre la zona.
+                        </p>
+                        <p>
+                                Torniquetes: Un torniquete es eficaz para controlar el sangrado potencialmente mortal de una de las extremidades. Aplica un torniquete si estás capacitado para hacerlo. Cuando llegue la ayuda de emergencia, explícales durante cuánto tiempo estuvo colocado el torniquete.
+                        </p>
+                        <p>
+                                Inmoviliza la parte del cuerpo lesionada en la mayor medida posible. Deja las vendas colocadas y traslada a la persona lesionada a una sala de emergencias lo antes posible.
+                        </p>
                     </div>
                 </div>
                 <div class="navigation-top">
@@ -54,37 +61,37 @@
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
                                 <div class="thumb">
-                                    <a href="/deas-en-lugares-publicos">
-                                        <img height="60px" width="60px" class="img-fluid" src="img/post/blog_3.png" alt="">
+                                    <a href="/el-fin-del-yeso-para-inmovilizar-extremidades">
+                                        <img height="60px" width="60px" class="img-fluid" src="img/post/blog_1.png" alt="">
                                     </a>
                                 </div>
                                 <div class="arrow">
-                                    <a href="/deas-en-lugares-publicos">
+                                    <a href="/el-fin-del-yeso-para-inmovilizar-extremidades">
                                         <span class="lnr text-white ti-arrow-left"></span>
                                     </a>
                                 </div>
                                 <div class="detials">
                                     <p>Anterior</p>
-                                    <a href="/deas-en-lugares-publicos">
-                                        <h4>DEAs en lugares públicos</h4>
+                                    <a href="/el-fin-del-yeso-para-inmovilizar-extremidades">
+                                        <h4>El fin del yeso para inmovilizar extremidades</h4>
                                     </a>
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
                                 <div class="detials">
                                     <p>Siguiente</p>
-                                    <a href="/stop-the-bleed">
-                                        <h4>Stop the bleed</h4>
+                                    <a href="/deas-en-lugares-publicos">
+                                        <h4>DEAs en lugares públicos</h4>
                                     </a>
                                 </div>
                                 <div class="arrow">
-                                    <a href="/stop-the-bleed">
+                                    <a href="/deas-en-lugares-publicos">
                                         <span class="lnr text-white ti-arrow-right"></span>
                                     </a>
                                 </div>
                                 <div class="thumb">
-                                    <a href="/stop-the-bleed">
-                                        <img height="60px" width="60px" class="img-fluid" src="img/post/blog_2.png" alt="">
+                                    <a href="/deas-en-lugares-publicos">
+                                        <img height="60px" width="60px" class="img-fluid" src="img/post/blog_3.png" alt="">
                                     </a>
                                 </div>
                             </div>
@@ -98,7 +105,7 @@
                             <a href="#">
                                 <h4>Jesús Soto</h4>
                             </a>
-                            <p>Descripción</p>
+                            <p>Pasante de la Primera Generación de Técnico en Urgencias Médicas por el Instituto La Luz, Delegación Durango de la Cruz Roja Mexicana.</p>
                         </div>
                     </div>
                 </div>
